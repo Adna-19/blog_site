2110f93d81aca36791226c6a57507e3a3f7396ad
Adna-19 <adilsaleem1398@gmail.com>
2021-03-07 11:09:32 +0500

    Added patterns for comment commentDelete and filters via tag

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,13 @@ app_name = 'blog'
 
 urlpatterns = [
   path('', views.BlogListView.as_view(), name='home'),
-  path('<slug:category_slug>/posts/', views.BlogListView.as_view(), name='list_by_category'),
+  path('category/<slug:category_slug>/posts/', views.BlogListView.as_view(), name='list_by_category'),
+  path('tag/<slug:tag_slug>/posts/', views.BlogListView.as_view(), name='list_by_tag'),
   path('post/<slug:slug>/', views.BlogDetailView.as_view(), name='post_detail'),
-  path('post/<slug:slug>/like/', views.BlogPostLikeDisLikeView.as_view(), name='like_dislike'),
+  path('post/<slug:slug>/like/', views.BlogPostLikeDisLikeView.as_view(), name='like_post'),
   path('create/', views.BlogPostCreateView.as_view(), name='create_post'),
   path('<int:pk>/edit/', views.BlogPostUpdateView.as_view(), name='update_post'),
   path('<slug:slug>/delete/', views.BlogPostDeleteView.as_view(), name='delete_post'),
+  path('comment/<int:comment_id>/delete/', views.BlogPostCommentDeleteView.as_view(), name='delete_comment'),
+  path('comment/<int:comment_id>/like/', views.PostCommentLikeDislikeView.as_view(), name='like_comment')
 ]
