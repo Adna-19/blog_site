9d8607678b8effb7130467962eb4e599cd3b402a
Adna-19 <adilsaleem1398@gmail.com>
2021-04-07 22:24:38 +0500

    Patterns for CMS added

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,13 @@ urlpatterns = [
   path('<int:pk>/edit/', views.BlogPostUpdateView.as_view(), name='update_post'),
   path('<slug:slug>/delete/', views.BlogPostDeleteView.as_view(), name='delete_post'),
   path('comment/<int:comment_id>/delete/', views.BlogPostCommentDeleteView.as_view(), name='delete_comment'),
-  path('comment/<int:comment_id>/like/', views.PostCommentLikeDislikeView.as_view(), name='like_comment')
+  path('comment/<int:comment_id>/like/', views.PostCommentLikeDislikeView.as_view(), name='like_comment'),
+
+  path('post/<slug:slug>/cms/details/', views.BlogPostContentsDetailView.as_view(), name='post_content_list'),
+  path('post/<slug:slug>/contents/<model_name>/create/', views.PostContentCreateUpdateView.as_view(), name='create_content'),
+  path('post/<slug:slug>/contents/<model_name>/<int:id>/update/', views.PostContentCreateUpdateView.as_view(), name='update_content'),
+  path('post/<slug:slug>/content/<model_name>/<int:id>/delete/', views.PostContentDeleteView.as_view(), name='delete_content'),
+  path('post/content/order/', views.PostContentOrderView.as_view(), name='order_content'),
 ]
 
 sitemaps = {
