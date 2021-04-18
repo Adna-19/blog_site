02369920e5578279287841ac646449264183de5e
Adna-19 <adilsaleem1398@gmail.com>
2021-04-18 12:49:24 +0500

    Newsletter subscription forms added

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Comment
+from .models import Comment, NewsletterSubscription
 
 class CommentForm(forms.ModelForm):
 	class Meta:
@@ -12,3 +12,32 @@ class CommentForm(forms.ModelForm):
 				'placeholder': 'Add a new comment...'
 			})
 		}
+
+class EmailSubscriptionForm(forms.ModelForm):
+  class Meta:
+    model = NewsletterSubscription
+    fields = ['email']
+    widgets = {
+    	'email': forms.TextInput(attrs={
+		    'class': 'form-control',
+		    'placeholder': 'Type your email address here...'
+		  })
+    }
+
+class EmailUnsubscriptionForm(forms.Form):
+  email = forms.EmailField(widget=forms.TextInput(
+    attrs={
+      'class': 'form-control w-50',
+      'placeholder': 'Type your email address here...'
+    }
+  ))
+
+# class EmailForm(forms.Form):
+#     subject = forms.CharField(label='Subject')
+#     to_email = forms.CharField(label='To')
+#     message = forms.CharField(label='Message', widget=forms.Textarea())
+
+#     def __init__(self, *args, **kwargs):
+#         super(EmailForm, self).__init__(*args, **kwargs)
+#         for field_name in self.fields.keys():
+#             self.fields[field_name].widget.attrs.update({'class': 'form-control'})
